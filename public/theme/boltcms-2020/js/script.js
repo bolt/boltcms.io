5e2f4305bb09dda9c8d3b65cac430ed1e1384ac7
Ivo Valchev <Ivo.V.Valchev@gmail.com>
2020-10-01 17:16:33 +0200

    Add click cta events

--- public/theme/boltcms-2020/js/script.js
+++ public/theme/boltcms-2020/js/script.js
@@ -5,3 +5,13 @@ window.addEventListener('scroll', function (e) {
       document.getElementById('header').classList.add('shadow');
    }
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+   document.querySelector('.try-it').addEventListener('click', function(){
+      _paq.push(['trackEvent', 'acquisition', 'install', 'try-it', window.location.pathname]);
+   });
+
+   document.querySelector('.install-bolt').addEventListener('click', function(){
+      _paq.push(['trackEvent', 'acquisition', 'install', 'install-bolt-cta', window.location.pathname]);
+   });
+});
